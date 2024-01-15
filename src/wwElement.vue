7f0c06a5bc1fd79f84aeb2bd8c2774723a06ad22
luka-weweb <luka@weweb.io>
2024-01-15 18:08:44 +0100

    aa:

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,139 +1,35 @@
-<template v-if="content">
-    <div class="ww-input-radio" :style="style" ww-responsive="ww-input-radio">
-        <div v-for="(option, index) in options" :key="index" class="ww-input-radio__container">
-            <wwLayoutItemContext v-if="option" :index="index" is-repeat>
-                <input
-                    :id="`${wwElementState.name}-${uniqueId}-${option.label}`"
-                    :value="option.value"
-                    :checked="option.value === value"
-                    class="ww-input-radio__radio"
-                    :class="{ editing: isEditing }"
-                    type="radio"
-                    :name="`${wwElementState.name}-${uniqueId}`"
-                    :required="content.required"
-                    @input="handleManualInput($event)"
-                />
-                <component
-                    :is="isEditing ? 'div' : 'label'"
-                    :for="`${wwElementState.name}-${uniqueId}-${option.label}`"
-                >
-                    <wwElement v-bind="content.choicesElement" :ww-props="{ text: option.label }" />
-                </component>
-            </wwLayoutItemContext>
-        </div>
-    </div>
+<template>
+    <button class="ww-radio" role="radio" aria-checked="true">
+        <div class="ww-radio-wrapper" :style="style"></div>
+    </button>
 </template>
 
 <script>
-import { computed } from 'vue';
-
 export default {
     props: {
         content: { type: Object, required: true },
-        /* wwEditor:start */
-        wwEditorState: { type: Object, required: true },
-        /* wwEditor:end */
-        wwElementState: { type: Object, required: true },
-        uid: { type: String, required: true },
-    },
-    emits: ['trigger-event', 'update:content:effect', 'update:sidepanel-content'],
-    setup(props) {
-        const { value: variableValue, setValue } = wwLib.wwVariable.useComponentVariable({
-            uid: props.uid,
-            name: 'value',
-            defaultValue: computed(() => (props.content.value === undefined ? '' : props.content.value)),
-        });
-        return { variableValue, setValue, uniqueId: wwLib.wwUtils.getUid() };
     },
     computed: {
-        isEditing() {
-            /* wwEditor:start */
-            return this.wwEditorState.editMode === wwLib.wwEditorHelper.EDIT_MODES.EDITION;
-            /* wwEditor:end */
-            // eslint-disable-next-line no-unreachable
-            return false;
-        },
-        value() {
-            if (!this.options.some(option => option.value === this.variableValue)) return null;
-            return this.variableValue;
-        },
-        options() {
-            if (!this.content.options) return [];
-            let data = wwLib.wwCollection.getCollectionData(this.content.options) || [];
-            if (data && !Array.isArray(data) && typeof data === 'object') {
-                data = new Array(data);
-            } else if ((data && !Array.isArray(data)) || typeof data !== 'object') {
-                return [];
-            }
-
-            return data
-                .filter(item => !!item)
-                .map(item => {
-                    if (typeof item !== 'object') return { label: item, value: item };
-                    return {
-                        label: wwLib.wwLang.getText(_.get(item, this.content.labelField || 'label') || ''),
-                        value: _.get(item, this.content.valueField || 'value'),
-                    };
-                });
-        },
         style() {
             return {
-                flexDirection: this.content.direction,
-                justifyContent: this.content.justifyContent,
-                alignItems: this.content.alignItems,
-                rowGap: this.content.rowGap,
-                columnGap: this.content.columnGap,
-                flexWrap:
-                    this.content.direction === 'column'
-                        ? 'nowrap'
-                        : this.content.flexWrap === false
-                        ? 'nowrap'
-                        : 'wrap',
+                backgroundColor: this.content.color,
+                width: this.content.size,
+                height: this.content.size,
             };
-        },
-    },
-    watch: {
-        /* wwEditor:start */
-        'wwEditorState.boundProps.options'(isBind) {
-            if (!isBind) this.$emit('update:content:effect', { labelField: null, valueField: null });
-        },
-        /* wwEditor:end */
-        'content.value'(newValue) {
-            newValue = `${newValue}`;
-            if (newValue === this.value) return;
-            this.setValue(newValue);
-            this.$emit('trigger-event', { name: 'initValueChange', event: { value: newValue } });
-        },
-    },
-    methods: {
-        handleManualInput(event) {
-            const value = event.target.value;
-            if (value === this.value) return;
-            this.setValue(value);
-            this.$emit('trigger-event', { name: 'change', event: { domEvent: event, value } });
         },
     },
 };
 </script>
 
-<style lang="scss" scoped>
-.ww-input-radio {
+<style scoped lang="scss">
+.ww-radio {
     display: flex;
+    justify-content: center;
+    align-items: center;
+}
 
-    &__container {
-        padding: 0.4rem 0;
-        display: flex;
-        flex-wrap: nowrap;
-        align-items: center;
-    }
-    &__radio {
-        outline: none;
-        margin-right: 0.4rem;
-        /* wwEditor:start */
-        &.editing {
-            pointer-events: none;
-        }
-        /* wwEditor:end */
-    }
+.ww-radio-wrapper {
+    border-radius: 50%;
+    display: block;
 }
 </style>
